a352882ace703c3d9098c1d16747840d39dbc4c1
Alen Magdic <alen.magdic123@gmail.com>
2020-09-22 19:22:20 +0200

    Set spinner adapter to hex formatter spinner

--- app/src/main/java/com/troido/hexinput/app/MainActivity.kt
+++ app/src/main/java/com/troido/hexinput/app/MainActivity.kt
@@ -2,11 +2,20 @@ package com.troido.hexinput.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.aconno.hexinputlib.formatter.HexFormatters
 import com.aconno.hexinputlib.setContentViewWithHexKeyboardAutoAdded
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentViewWithHexKeyboardAutoAdded(R.layout.activity_main,true)
+
+        hex_formatter.adapter = ArrayAdapter<HexFormatters.FormatterType>(
+            this,
+            android.R.layout.simple_spinner_item,
+            HexFormatters.FormatterType.values()
+        )
     }
 }
