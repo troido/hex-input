2b1ea4f10e25d6a6c569ba8f75c23113e981b9fe
Alen Magdic <alen.magdic123@gmail.com>
2020-09-23 12:49:23 +0200

    Implemented formatter selection for demo hex input

--- app/src/main/java/com/troido/hexinput/app/MainActivity.kt
+++ app/src/main/java/com/troido/hexinput/app/MainActivity.kt
@@ -2,8 +2,11 @@ package com.troido.hexinput.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.aconno.hexinputlib.formatter.HexFormatters
+import com.aconno.hexinputlib.formatter.PlainValuesHexFormatter
 import com.aconno.hexinputlib.setContentViewWithHexKeyboardAutoAdded
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,10 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentViewWithHexKeyboardAutoAdded(R.layout.activity_main,true)
 
+        simple_hex_input.setFormatter(PlainValuesHexFormatter())
+
         hex_formatter.adapter = ArrayAdapter<HexFormatters.FormatterType>(
             this,
             android.R.layout.simple_spinner_item,
             HexFormatters.FormatterType.values()
         )
+        hex_formatter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                demo_hex_input.setFormatter(PlainValuesHexFormatter())
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedFormatterType = HexFormatters.FormatterType.values()[position]
+                val formatter = HexFormatters.getFormatter(selectedFormatterType)
+                demo_hex_input.setFormatter(formatter)
+            }
+        }
+
+
     }
 }
